96de3114be8a2248dff72527ddd84ac4456b0635
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-02-08 17:22:54 -0500

    flags

--- cmd/web/parse_flags.go
+++ cmd/web/parse_flags.go
@@ -10,6 +10,7 @@ func parseFlags(app *config.AppConfig) {
 
 	flag.StringVar(&app.WebsiteName, "website_name", "My Cool Website", "the name you want to give to your website")
 	flag.StringVar(&app.WebsiteAddress, "website_url", "http://localhost", "the base url for the site")
+	flag.BoolVar(&app.IsProduction, "is_production", false, "set environment(development|production)")
 	flag.Parse()
 
 }
